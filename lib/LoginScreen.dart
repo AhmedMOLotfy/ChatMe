2a55fa0194023e191594e302f9c546839d6a2ee4
programmerahmedlotfy2001 <62852398+programmerahmedlotfy2001@users.noreply.github.com>
2021-02-07 14:23:16 +0200

    SharedPreferences to handle Sign In with google

--- lib/LoginScreen.dart
+++ lib/LoginScreen.dart
@@ -1,6 +1,8 @@
 import 'package:chatme/utilizes.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -19,6 +21,8 @@ class _LoginScreenState extends State<LoginScreen> {
   SharedPreferences prefs; //SharedPreferences
   bool isLoading = false;
 
+  User currentUser;
+
   Future<Null> handleSignIn() async {
     prefs = await SharedPreferences.getInstance();
     this.setState(() {
@@ -31,8 +35,57 @@ class _LoginScreenState extends State<LoginScreen> {
       idToken: googleAuth.idToken,
       accessToken: googleAuth.accessToken,
     );
+    //User that use now
     User firebaseUser =
         (await firebaseAuth.signInWithCredential(credential)).user;
+    if (firebaseUser != null) {
+      // Check is already sign up
+      QuerySnapshot result = await FirebaseFirestore.instance
+          .collection('users')
+          .where('id', isEqualTo: firebaseUser.uid)
+          .get();
+      final List<DocumentSnapshot> documents = result.docs;
+      if (documents.length == 0) {
+        // Update data to server if new user
+        FirebaseFirestore.instance
+            .collection('users')
+            .doc(firebaseUser.uid)
+            .set({
+          'nickname': firebaseUser.displayName,
+          'photoUrl': firebaseUser.photoURL,
+          'id': firebaseUser.uid,
+          'createdAt': DateTime.now().toString(),
+          'chattingWith': null
+        });
+
+        // Write data to local
+        currentUser = firebaseUser;
+        await prefs.setString('id', currentUser.uid);
+        await prefs.setString('nickname', currentUser.displayName);
+        await prefs.setString('photoUrl', currentUser.photoURL);
+      } else {
+        // Write data to local
+        await prefs.setString('id', documents[0].data()['id']);
+        await prefs.setString('nickname', documents[0].data()['nickname']);
+        await prefs.setString('photoUrl', documents[0].data()['photoUrl']);
+        await prefs.setString('aboutMe', documents[0].data()['aboutMe']);
+      }
+      Fluttertoast.showToast(msg: "Sign in success");
+      this.setState(() {
+        isLoading = false;
+      });
+
+      // Navigator.push(
+      //     context,
+      //     MaterialPageRoute(
+      //         builder: (context) =>
+      //             HomeScreen(currentUserId: firebaseUser.uid)));
+    } else {
+      Fluttertoast.showToast(msg: "Sign in fail");
+      this.setState(() {
+        isLoading = false;
+      });
+    }
   }
 
   @override
